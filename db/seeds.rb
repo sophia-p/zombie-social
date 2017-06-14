394720f7be78e0d21224081f10be49488a53b6a3
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-14 01:00:51 -0400

    add paperclip

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Zombie.destroy_all
+Zombie.create(username:"livingit", email:"livmoore", password:"password", image:File.new("app/assets/images/Gwen.jpg"))
