54bca9215c4c758c3d17ed0ba3f610f4168e9c2c
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-14 00:25:56 -0400

    add new user form

--- app/models/zombie.rb
+++ app/models/zombie.rb
@@ -3,4 +3,6 @@ class Zombie < ApplicationRecord
 	validates :email, :username, presence: true
 	validates :email, :username, :password, length:{minimum:6}
 	validates_confirmation_of :password
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end	
