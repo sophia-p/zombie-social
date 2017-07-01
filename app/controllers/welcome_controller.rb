c3170d67b2e8a2733b921da284aea1d2ac2a9f3f
Sophia Pignatelli <pignatelli@fordham.edu>
2017-06-30 23:37:48 -0400

    add time ago to activity

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
 	def index
 		@zombies = Zombie.all
 		@posts = Post.all
-		@activities = PublicActivity::Activity.all
+		@activities = PublicActivity::Activity.order("created_at desc")
 	end
 end
