04916498769a9d688a0b359a4347c4170e9c02d0
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-04 00:22:50 -0400

    add login redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
 	def index
+		if current_user
 		@zombies = Zombie.all
 		@posts = Post.all
 		@activities = PublicActivity::Activity.order(created_at: :desc)
+		
+		else
+			redirect_to :controller => 'sessions', :action => 'new'
+ 
+		end
 	end
 end
