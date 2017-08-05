0820b3e2cdd16d4541ba29c005e11967b0046f6d
Sophia Pignatelli <pignatelli@fordham.edu>
2017-08-04 23:27:29 -0400

    edit welcome test

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe WelcomeController, type: :controller do
-	describe "GET #index" do
-		it "returns http success" do
-			get :index
-			expect(response).to render_template(:index)
+	describe "GET login page" do
+		it "redirects if user not signed in" do
+			expect(response).to redirect_to '/sessions/new'
 		end
 	end
 end
